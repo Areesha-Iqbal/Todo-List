01601cd06c2dcb31260ded3185efb3e4534a6fa7
Areesha-Iqbal <areeshaiqbal319@gmail.com>
2024-03-31 13:35:39 -0700

    Adding a CLI based Todo Application

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(
+  chalk.magenta.bold("\n\tWelcome to 'Areesha Iqbal' - Todo List Application\n")
+);
+
+let todos: string[] = [];
+let condition = true;
+while (condition) {
+  let addTask = await inquirer.prompt([
+    {
+      name: "todo",
+      type: "input",
+      message: chalk.yellow.italic("What do you want to add in your Todo?"),
+    },
+    {
+      name: "addMore",
+      type: "confirm",
+      message: chalk.blueBright.italic("Do you want to add more Todo?"),
+      default: "false",
+    },
+  ]);
+
+  todos.push(addTask.todo);
+  condition = addTask.addMore;
+  console.log(todos);
+}
